48f2f01bdd25631827980d898b92e440ee91c3bf
Egor <egor.andrianovv@gmail.com>
2025-09-05 01:28:00 +0300

    Update __init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,3 +1,13 @@
-"""
-Утилиты
-"""
+from .pricing_utils import (
+    calculate_months_from_days,
+    get_remaining_months,
+    calculate_prorated_price,
+    format_period_description
+)
+
+__all__ = [
+    'calculate_months_from_days',
+    'get_remaining_months', 
+    'calculate_prorated_price',
+    'format_period_description'
+]
