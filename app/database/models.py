637af5c21687451fa6e30b56853b7713b3fc907b
Egor <egor.andrianovv@gmail.com>
2025-09-05 17:20:28 +0300

    Update models.py

--- app/database/models.py
+++ app/database/models.py
@@ -125,7 +125,7 @@ class User(Base):
     trojan_password = Column(String(255), nullable=True)
     vless_uuid = Column(String(255), nullable=True)
     ss_password = Column(String(255), nullable=True)
-    as_made_first_topup: Column(Boolean, default=False, nullable=False)
+    has_made_first_topup: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     
     @property
     def balance_rubles(self) -> float:
