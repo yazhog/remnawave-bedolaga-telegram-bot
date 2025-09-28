dd68b2b7887e4f68b1bffc6526a1ec72ea771d48
Egor <egor.andrianovv@gmail.com>
2025-09-28 03:27:02 +0300

    Revert "Make Remnawave settings optional by default"

--- app/config.py
+++ app/config.py
@@ -53,8 +53,8 @@ class Settings(BaseSettings):
     
     REDIS_URL: str = "redis://localhost:6379/0"
     
-    REMNAWAVE_API_URL: Optional[str] = None
-    REMNAWAVE_API_KEY: Optional[str] = None
+    REMNAWAVE_API_URL: str = ""
+    REMNAWAVE_API_KEY: str = ""
     REMNAWAVE_SECRET_KEY: Optional[str] = None
 
     REMNAWAVE_USERNAME: Optional[str] = None
@@ -68,7 +68,7 @@ class Settings(BaseSettings):
     TRIAL_DEVICE_LIMIT: int = 2
     DEFAULT_TRAFFIC_LIMIT_GB: int = 100
     DEFAULT_DEVICE_LIMIT: int = 1
-    TRIAL_SQUAD_UUID: Optional[str] = None
+    TRIAL_SQUAD_UUID: str = ""
     DEFAULT_TRAFFIC_RESET_STRATEGY: str = "MONTH"
     MAX_DEVICES_LIMIT: int = 20
     
