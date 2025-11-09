a043fc0e4675552b2deca63b1920762dcb305abe
Egor <egor.andrianovv@gmail.com>
2025-11-09 04:36:33 +0300

    feat: add backend support for server groups

--- app/states.py
+++ app/states.py
@@ -183,6 +183,13 @@ class SquadMigrationStates(StatesGroup):
     confirming = State()
 
 
+class ServerGroupStates(StatesGroup):
+    waiting_for_name = State()
+    selecting_servers = State()
+    editing_name = State()
+    editing_servers = State()
+
+
 class RemnaWaveSyncStates(StatesGroup):
     waiting_for_schedule = State()
 
