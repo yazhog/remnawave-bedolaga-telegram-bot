861ffe5424f6be28682c8d729e29f36fe2e59cb4
firewookie <firewookie@yandex.ru>
2026-03-06 12:36:32 +0500

    fix migration

--- migrations/alembic/versions/0015_add_riopay_payments.py
+++ migrations/alembic/versions/0015_add_riopay_payments.py
@@ -11,7 +11,7 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = '0016'
+revision: str = '0014'
 down_revision: Union[str, None] = '0015'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
